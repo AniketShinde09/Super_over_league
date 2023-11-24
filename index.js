2af05bb225f6f0b083a793e49894db29cf33f779
aniket <aashinde9999@gamail.com>
2023-11-24 09:24:19 +0000

    aniket commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="container">
+    <div>
+      <h1 className="main-heading">Super Over League</h1>
+    </div>
+    <div className="img-container">
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      ></img>
+      <img
+        className="img"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
